ee1de524231150206408fabc91fbf2e57386f3d8
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2024-12-25 11:55:55 +0530

    added media settings

--- til/urls.py
+++ til/urls.py
@@ -17,9 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include, re_path
 from feed import urls as feed_urls
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(feed_urls, namespace='feed')),
     re_path("", include("allauth.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# static will only be used during development
