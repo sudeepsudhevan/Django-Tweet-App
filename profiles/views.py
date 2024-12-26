3686c83d9858f54830655736db3b096706acc596
Sudeep Sudhevan <31392327+sudeepsudhevan@users.noreply.github.com>
2024-12-26 11:33:24 +0530

    created follower app

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.views.generic import DetailView
 
+from feed.models import Post
 from .models import Profile
 
 
@@ -11,3 +12,9 @@ class ProfileDetailView(DetailView):
     context_object_name = 'user'
     slug_field = 'username'
     slug_url_kwarg = 'username'
+
+    def get_context_data(self, **kwargs):
+        user = self.get_object()
+        context = super().get_context_data(**kwargs)
+        context['total_posts'] = Post.objects.filter(author=user).count()  # get the total number of posts
+        return context
--- til/settings.py
+++ til/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
     'feed',
     'profiles',
+    'followers',
 
     'allauth',
     'allauth.account',
